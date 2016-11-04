c57f7566c96fc9eca4ef13a834049fd55c344e64
Neal Wolff <ecc.neal@gmail.com>
2016-11-04 01:13:09 -0400

    integreated picasso into the project for managing images within in the lists. Added listings class to save listing data into, added a custom adapter for the listing view to display the customlayout view into the listings. currently there is just test listings that populate the list, pulling image files from a remote server.

---
+++ app/src/main/java/com/example/we00401/guntest2/customAdapter.java
@@ -0,0 +1,49 @@
+package com.example.we00401.guntest2;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+public class customAdapter extends ArrayAdapter<listings> {
+    ArrayList<listings> listing;
+    Context context;
+    int resource;
+
+    public customAdapter(Context context, int resource, ArrayList<listings> listing) {
+        super(context, resource, listing);
+        this.listing = listing;
+        this.context = context;
+        this.resource = resource;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (convertView == null){
+            LayoutInflater layoutInflater = (LayoutInflater) getContext()
+                    .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.customlayout, null, true);
+
+        }
+        listings listing = getItem(position);
+
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.imageViewListing);
+        Picasso.with(context).load(listing.getImage()).into(imageView);
+
+        TextView txtName = (TextView) convertView.findViewById(R.id.txtName);
+        txtName.setText(listing.getName());
+
+        TextView txtPrice = (TextView) convertView.findViewById(R.id.txtPrice);
+        txtPrice.setText(listing.getPrice());
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/we00401/guntest2/search.java
+++ app/src/main/java/com/example/we00401/guntest2/search.java
@@ -13,16 +13,43 @@ import android.widget.EditText;
 import android.view.View.OnClickListener;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.widget.ListView;
+import java.util.ArrayList;
 
 public class search extends AppCompatActivity {
     private GestureDetectorCompat gestureObject;
     boolean first = true;
+
+    //array list of listings
+    ArrayList<listings> arrayList;
+    ListView lv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        arrayList = new ArrayList<>();
+        lv = (ListView) findViewById(R.id.foundView);
+
+        listings kek = new listings("http://i0.kym-cdn.com/photos/images/facebook/000/617/382/1d8.png","topkek","$100");
+        listings kek2 = new listings("https://c.thumbs.redditmedia.com/tSkMnJtm4xt_e4QD.png","kek","$1340");
+        arrayList.add(kek);
+        arrayList.add(kek2);
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                customAdapter adapter = new customAdapter(
+                        getApplicationContext(), R.layout.customlayout, arrayList
+                );
+                lv.setAdapter(adapter);
+            }
+        });
+
+
 
         gestureObject = new GestureDetectorCompat(this, new LearnGesture());
 
@@ -52,7 +79,7 @@ public class search extends AppCompatActivity {
 
                     alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
-                            // here you can add functions
+
                         }
                     });
 
