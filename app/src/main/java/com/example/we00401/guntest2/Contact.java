d91e0776334d66008db494e314c004cd7652d08b
Joshua <jk04246@georgiasouthern.edu>
2016-12-07 17:55:13 -0500

    added getters and setters

--- app/src/main/java/com/example/we00401/guntest2/Contact.java
+++ app/src/main/java/com/example/we00401/guntest2/Contact.java
@@ -10,6 +10,8 @@ public class Contact {
 //    int id;
     String name, email, uname, pass, nameSave;
 
+    String imageurl, url, title, price;
+
 //    public void setId(int id) {
 //        this.id=id;
 //    }
@@ -18,39 +20,53 @@ public class Contact {
 //        return this.id;
 //    }
 
+
     public void setName(String name) {
         this.name = name;
     }
-
     public String getName() {
         return this.name;
     }
 
+
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getEmail() {
         return this.email;
     }
 
+
     public void setUname(String uname) {
         this.uname = uname;
     }
-
     public String getUname() {
         return this.uname;
     }
 
+
     public void setPass(String pass) {
         this.pass = pass;
     }
-
     public String getPass() {
         return this.pass;
     }
 
-    public void setNameSave() {this.nameSave = nameSave; }
 
-    public String getNameSave() {return this.nameSave; }
+    public void setNameSave(String nameSave) { this.nameSave = nameSave; }
+    public String getNameSave() { return this.nameSave; }
+
+
+    public void setImageUrl(String imageurl) { this.imageurl = imageurl; }
+    public String getImageUrl(){ return this.imageurl; }
+
+    public void setPrice(String price) { this.price = price; }
+    public String getPrice(){ return this.price; }
+
+    public void setTitle(String title){ this.title = title; }
+    public String getTitle(){ return this.title; }
+
+    public void setUrl(String url) { this.url = url; }
+    public String getUrl(){ return this.url; }
+
 }
